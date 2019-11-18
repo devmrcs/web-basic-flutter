8ee7320c81c5ecf26e6c0bc207a9eab477c52dd2
Marcos Souza <marcos13@outlook.com>
2019-11-18 08:29:14 -0300

    :rocket:  Home view to firebase

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:web_begin/widgets/call_to_action/call_to_action.dart';
+import 'package:web_begin/widgets/centered_view/centered_view.dart';
+import 'package:web_begin/widgets/course_details/course_details.dart';
 import 'package:web_begin/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,10 +9,24 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          NavigationBar(),
-        ],
+      body: CenteredView(
+        child: Column(
+          children: <Widget>[
+            NavigationBar(),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CourseDetails(),
+                  Expanded(
+                    child: Center(
+                      child: CallToAction('Join Course'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
